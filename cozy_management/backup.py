e69bb792fe4e0e5266e8593455cf290e975f7d3d
Nicolas Ledez <github.public@ledez.net>
2016-03-31 21:06:07 +0000

    cozy_management/backup.py refactoring

--- cozy_management/backup.py
+++ cozy_management/backup.py
@@ -10,9 +10,15 @@ from . import couchdb
 
 BACKUPS_PATH = '/var/lib/cozy/backups'
 
-database_dir = couchdb.get_database_dir()
-#get the first value that should be the couchdb path
-COUCHDB_PATH = database_dir[list(database_dir)[0]]
+
+def _get_couchdb_path():
+    '''
+        Get CouchDB .couch path
+    '''
+    database_dir = couchdb.get_database_dir()
+
+    # get the first value that should be the couchdb path
+    return database_dir[list(database_dir)[0]]
 
 
 def backup():
@@ -22,13 +28,17 @@ def backup():
     if not os.path.isdir(BACKUPS_PATH):
         print 'Need to create {}'.format(BACKUPS_PATH)
         os.makedirs(BACKUPS_PATH, 0700)
+    couchdb_path = _get_couchdb_path()
+
     timestamp = time.strftime("%Y-%m-%d-%H-%M-%S", time.gmtime())
-    cmd = 'tar cvzf {}/cozy-{}.tgz'
+    cmd = 'tar cvzf {backups_path}/cozy-{timestamp}.tgz'
     cmd += ' --exclude stack.token'
     cmd += ' --exclude couchdb.login'
     cmd += ' --exclude self-hosting.json'
-    cmd += ' /etc/cozy /usr/local/var/cozy {couchdb_path}/cozy.couch'.format(couchdb_path=COUCHDB_PATH)
-    cmd = cmd.format(BACKUPS_PATH, timestamp)
+    cmd += ' /etc/cozy /usr/local/var/cozy {couchdb_path}/cozy.couch'
+    cmd = cmd.format(backups_path=BACKUPS_PATH,
+                     timestamp=timestamp,
+                     couchdb_path=couchdb_path)
     helpers.cmd_exec(cmd, show_output=True)
     print 'Backup file: {}/cozy-{}.tgz'.format(BACKUPS_PATH, timestamp)
 
@@ -41,14 +51,16 @@ def restore(backup_file):
     if not os.path.isfile(backup_file) and not os.path.islink(backup_file):
         print 'Missing backup file: {}'.format(backup_file)
     else:
+        couchdb_path = _get_couchdb_path()
+
         print 'Restore Cozy:'
         cmd = 'supervisorctl stop cozy-controller ; sleep 10'
         cmd += ' ; service couchdb stop ; service nginx stop'
-        cmd += ' ; rm -rf {couchdb_path}/.cozy_design'.format(couchdb_path=COUCHDB_PATH)
-        cmd += ' {couchdb_path}/_replicator.couch'.format(couchdb_path=COUCHDB_PATH)
-        cmd += ' ; tar xvzf {} -C /'
+        cmd += ' ; rm -rf {couchdb_path}/.cozy_design'
+        cmd += ' {couchdb_path}/_replicator.couch'
+        cmd += ' ; tar xvzf {backup_file} -C /'
         cmd += ' ; service couchdb start ; service nginx start'
-        cmd = cmd.format(backup_file)
+        cmd = cmd.format(backup_file=backup_file, couchdb_path=couchdb_path)
         helpers.cmd_exec(cmd, show_output=True)
         helpers.wait_couchdb(10)
         cmd = 'supervisorctl start cozy-controller'
